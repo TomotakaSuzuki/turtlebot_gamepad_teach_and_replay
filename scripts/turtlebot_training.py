5fa02be42dacbcf6fe540360bd614e03b659377d
TomotakaSuzuki <s1526067nu@s.chibakoudai.jp>
2017-12-13 02:34:10 +0900

    fix topic name

--- scripts/turtlebot_training.py
+++ scripts/turtlebot_training.py
@@ -9,9 +9,15 @@ class JoyTwist(object):
     def __init__(self):
         self._btm_sub = rospy.Subscriber('/buttons', ButtonValues, self.button_callback, queue_size=1)
         self._joy_sub = rospy.Subscriber('/joy', Joy, self.joy_callback, queue_size=1)
-        self._vel_pub = rospy.Publisher('/cmd_vel_mux/imput/teleop', Twist, queue_size=1) #cmd_vel_mux/input/navi
+        self._vel_pub = rospy.Publisher('/cmd_vel_mux/input/teleop', Twist, queue_size=1)
 
+        self.level = 1
         self.on = False
+
+    def limitter(self, lvl):
+        if lvl <= 0: return 1
+        if lvl >= 6: return 5
+        return lvl
        
     def button_callback(self, btm_msg):
         self.on = btm_msg.training 
@@ -20,11 +26,18 @@ class JoyTwist(object):
         if not self.on:
             return
 
+        if joy_msg.buttons[7] == 1: self.level += 1
+        if joy_msg.buttons[6] == 1: self.level -= 1
+        self.level = self.limitter(self.level)
+
         if joy_msg.buttons[0] == 1:
             twist = Twist()
-            twist.linear.x = joy_msg.axes[1] * 0.2
-            twist.angular.z = joy_msg.axes[0] * 3.14/32
+            twist.linear.x = joy_msg.axes[1] * 0.2 * self.level
+            twist.angular.z = joy_msg.axes[0] * 3.14/32 * (self.level + 15)
             self._vel_pub.publish(twist)
+
+        if joy_msg.axes[1] == joy_msg.axes[0] == 0:
+            self.level -= 1
 
 if __name__ == '__main__':
     rospy.init_node('turtlebot_training')
